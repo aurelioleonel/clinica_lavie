313034bc3e466e917f68e8ed9a810bf5f2531e42
MarcosAurelio <aurelioleonel@gmail.com>
2022-10-21 15:56:01 -0300

    Ateração e stilização no arquivo Reademe

--- Documentacao/clinica_lavie.sql
+++ Documentacao/clinica_lavie.sql
@@ -1,77 +1,89 @@
--- MySQL Workbench Forward Engineering
+-- MySQL dump 10.13  Distrib 8.0.21, for Win64 (x86_64)
+--
+-- Host: localhost    Database: clinica_lavie
+-- ------------------------------------------------------
+-- Server version	8.0.21
 
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- -----------------------------------------------------
--- Schema clinica_lavie
--- -----------------------------------------------------
+--
+-- Table structure for table `atendimentos`
+--
 
--- -----------------------------------------------------
--- Schema clinica_lavie
--- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `clinica_lavie` DEFAULT CHARACTER SET utf8 ;
-USE `clinica_lavie` ;
-
--- -----------------------------------------------------
--- Table `clinica_lavie`.`psicologos`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `clinica_lavie`.`psicologos` (
-  `id_psicologo` INT NOT NULL AUTO_INCREMENT,
-  `nome` VARCHAR(200) NOT NULL,
-  `email` VARCHAR(100) NOT NULL,
-  `senha` VARCHAR(245) NOT NULL,
-  `apresentacao` TEXT NULL,
-  `createdAt` DATE NULL,
-  `updatedAt` DATE NULL,
-  PRIMARY KEY (`id_psicologo`),
-  UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `clinica_lavie`.`pacientes`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `clinica_lavie`.`pacientes` (
-  `id_paciente` INT NOT NULL AUTO_INCREMENT,
-  `nome` VARCHAR(200) NOT NULL,
-  `email` VARCHAR(100) NOT NULL,
-  `idade` DATE NOT NULL,
-  `createdAt` DATE NULL,
-  `updatedAt` DATE NULL,
-  PRIMARY KEY (`id_paciente`),
-  UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `clinica_lavie`.`atendimentos`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `clinica_lavie`.`atendimentos` (
-  `id_atendimento` INT NOT NULL AUTO_INCREMENT,
-  `data_atendimento` DATE NULL,
-  `observacao` TEXT NULL,
-  `id_psicologo` INT NOT NULL,
-  `id_paciente` INT NOT NULL,
-  `createdAt` DATE NULL,
-  `updatedAt` DATE NULL,
+DROP TABLE IF EXISTS `atendimentos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `atendimentos` (
+  `id_atendimento` int NOT NULL AUTO_INCREMENT,
+  `data_atendimento` date DEFAULT NULL,
+  `observacao` text,
+  `id_psicologo` int NOT NULL,
+  `id_paciente` int NOT NULL,
+  `createdAt` date DEFAULT NULL,
+  `updatedAt` date DEFAULT NULL,
   PRIMARY KEY (`id_atendimento`),
-  INDEX `fk_atendimentos_psicologos_idx` (`id_psicologo` ASC) VISIBLE,
-  INDEX `fk_atendimentos_pacientes1_idx` (`id_paciente` ASC) VISIBLE,
-  CONSTRAINT `fk_atendimentos_psicologos`
-    FOREIGN KEY (`id_psicologo`)
-    REFERENCES `clinica_lavie`.`psicologos` (`id_psicologo`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_atendimentos_pacientes1`
-    FOREIGN KEY (`id_paciente`)
-    REFERENCES `clinica_lavie`.`pacientes` (`id_paciente`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+  KEY `fk_atendimentos_psicologos_idx` (`id_psicologo`),
+  KEY `fk_atendimentos_pacientes1_idx` (`id_paciente`),
+  CONSTRAINT `fk_atendimentos_pacientes1` FOREIGN KEY (`id_paciente`) REFERENCES `pacientes` (`id_paciente`),
+  CONSTRAINT `fk_atendimentos_psicologos` FOREIGN KEY (`id_psicologo`) REFERENCES `psicologos` (`id_psicologo`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Table structure for table `pacientes`
+--
 
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+DROP TABLE IF EXISTS `pacientes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `pacientes` (
+  `id_paciente` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(200) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `idade` date NOT NULL,
+  `createdAt` date DEFAULT NULL,
+  `updatedAt` date DEFAULT NULL,
+  PRIMARY KEY (`id_paciente`),
+  UNIQUE KEY `email_UNIQUE` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `psicologos`
+--
+
+DROP TABLE IF EXISTS `psicologos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `psicologos` (
+  `id_psicologo` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(200) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `senha` varchar(245) NOT NULL,
+  `apresentacao` text,
+  `createdAt` date DEFAULT NULL,
+  `updatedAt` date DEFAULT NULL,
+  PRIMARY KEY (`id_psicologo`),
+  UNIQUE KEY `email_UNIQUE` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-10-20 18:01:34
--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # Documentação da API 
 https://documenter.getpostman.com/view/23922555/2s84Diy5Rh
+
+
+# Clínica La Vie - Saúde Mental
+<h2>Projeto Clínica La Vie, desafio da Gama Academy - xp45</h2>
+<p>
+Um grupo de psicólogos se juntaram e criaram a clínica La Vie -
+Saúde Mental que oferece diversos tipos de terapia.
+Para ajudar nos atendimentos, eles precisam de uma API que
+permita criar registros de psicólogos, pacientes e prontuários.
+Em uma conversa com os Front-end e os PO foram decididos
+alguns grupos de endpoints que devem ser criad
+</p>
+
+<h2>O objetivo do projeto é atender a nescessidade da clínica
+Foi utilizado as ferramentas do Html, Css, javascript, nodejs e o VScode.
+<div>
+  <h2>Eventos do projeto </h2>
+    <ul>
+        <li>Fazer Lgin gerando um token</li>
+        <li>Fazer cadastro de pacientes</li>
+        <li>Fazer cadastro de psicólogos</li>
+        <li>Fazer cadastro de atendimentos</li>
+        <li>Recursos operacionais</li>
+        <ul>
+           <li>Listar todos os pacientes</li>
+           <li>Listar todos os psicólogs</li>
+           <li>Cadastrar</li>
+           <li>Atualizar</li>
+           <li>Excluir</li>
+        </ul>
+        <li>Recursos extras</li>
+        <ul>
+           <li>Total de pacientes</li>
+           <li>Total de psicólogos</li>
+           <li>M´dia de atendimentos do psicólogo</li>
+        </ul>
+    </ul>
+    
+</div>
+<p>Criadores do projeto: Marcos Aurélio, Marilene Correa
+<a>Documentação da API <a href="https://documenter.getpostman.com/view/23922555/2s84Diy5Rh"
+ target="_blank">CLIQUE AQUI!</a>
+</h2>
+
